f1538663fc95efe374f385e440cfbda39262dbb6
Joash Jenushan <137409476+Joash-Jenushan@users.noreply.github.com>
2023-08-11 14:31:37 +0530

    Add files via upload

---
+++ info.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+clear
+
+# Create the "netMap" text using figlet and color with lolcat
+figlet -f slant "netMap" | lolcat -p 2.5 -S 20
+
+# Print the description
+echo -e "Using \033[;34mnetMap\033[0m will allow you to scan and find Devices on your Network using nmap and adb. I hope \033[;34mnetMap\033[0m will be a good tool for scanning."
+
+# Print the version details
+echo -e "\033[;34mnetMap\033[0m has two versions:"
+echo -e "    1. \033[;34mnetMap-V1.py\033[0m"
+echo -e "    2. \033[;34mnetMap-V2.py\033[0m"
+
+# Print version descriptions
+echo -e "\033[;34mnetMap-V1.py\033[0m is the default script, which will direct you to the scanning process."
+echo -e "\033[;34mnetMap-V2.py\033[0m is the same script, but when you run this as root, it'll scan for tools called 'adb' and 'scapy'. If they're available on your device, you will be directed to the scanning process. If they're not available, don't worry, \033[;34mnetMap\033[0m v2.0 will install the requirements for you."
+
+
+# Print disclaimer in red bold font
+echo -e "\033[1;31mDisclaimer: This tool is for educational purposes only!\033[0m"
+
+# Print usage instructions
+echo -e "\n\033[;34mUsage:\033[0m"
+echo -e "chmod +x netMap_v1.0_2023.sh"
+echo -e "sudo ./netMap_v1.0_2023.sh"
+echo -e "Enter the IP range: \033[;34m<Your network IP>\033[0m"
+
+# Function to center-align text in the terminal
+center_text() {
+  termwidth=$(tput cols)
+  padding="$(printf '%0.1s' ' '{1..500})"
+  printf "%*.*s %s %*.*s\n" 0 $((($termwidth - 2 - ${#1}) / 2)) "$padding" "$1" 0 $((($termwidth - 1 - ${#1}) / 2)) "$padding"
+}
+
+# Call the function
+center_text "$1" "$2"
+
---
+++ netMapV1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import subprocess
+import pyfiglet
+
+def center_text(text):
+    termwidth = int(subprocess.check_output(['tput', 'cols']))
+    padding = ' ' * ((termwidth - len(text)) // 2)
+    return padding + text
+
+def create_banner():
+    banner_text = "\033[1;34m" + pyfiglet.Figlet().renderText("netMap.py") + "\033[0m"
+    note_text = "\033[;34m netMap 1.0 Will Scan Your Network IP Address and Show You the Connected Devices IP Address and MAC Address.\033[0m"
+    creator_text = "\033[1;32mCreated by. Joash Jenushan\033[0m"
+
+    print(center_text(banner_text))
+    print(center_text(note_text))
+    print(center_text(creator_text))
+
+if __name__ == "__main__":
+    create_banner()
+
+print("-----------------------------------------------------")
+
+from scapy.all import ARP, Ether, srp
+
+def scan(ip_range, interface):
+    # Create ARP request packet
+    arp = ARP(pdst=ip_range)
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = ether/arp
+    
+    # Send and receive packets
+    result = srp(packet, timeout=3, iface=interface, verbose=False)[0]
+    
+    devices = []
+    for sent, received in result:
+        if received.psrc != ip_range.split('/')[0]:  # Exclude router IP
+            devices.append({'ip': received.psrc, 'mac': received.hwsrc})
+    
+    return devices
+
+def main():
+    interface = "wlan0"  # interface 
+    ip_range = input("Enter the IP range: ")
+    
+    devices = scan(ip_range, interface)
+    
+    print("\nDevices on the network:")
+    print("-----------------------")
+    for idx, device in enumerate(devices, start=1):
+        print(f"Device {idx}:")
+        print(f"IP: {device['ip']}")
+        print(f"MAC: {device['mac']}\n")
+
+if __name__ == "__main__":
+    main()
