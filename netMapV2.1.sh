04b08f211adf93115dfbe04f98ba70b4a116bfeb
Joash Jenushan <137409476+Joash-Jenushan@users.noreply.github.com>
2023-08-13 02:13:46 +0530

    Update and rename netMap v0.5.sh to netMapV2.1.sh

--- netMapV2.1.sh
+++ netMapV2.1.sh
@@ -3,10 +3,10 @@
 clear
 
 # Create the "netMap v0.5" heading using figlet and color with lolcat
-figlet -f slant "netMap v0.5" | lolcat -p 1.5 -S 50
+figlet -f slant "netMap V2.1" | lolcat -p 1.5 -S 50
 
 # Print description
-echo "netMap v0.5 will scan devices on a network IP address. This will run on interface eth0."
+echo "netMap V2.1 will scan devices on a network IP address. This will run on interface eth0."
 
 # Print disclaimer in red color
 echo -e "\033[1;31mDisclaimer: Only for Educational Purposes\033[0m"
